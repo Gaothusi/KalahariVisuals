aade2b73b76827c20a37f972ce7c79b604b0599b
WRG <info@Afrovisuals.com>
2016-05-11 23:12:03 -0600

    testing git

---
+++ KalahariVisuals/KalahariVisuals.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KalahariVisuals.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
